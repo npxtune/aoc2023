f60d9d9c859d9d333d76a18929cd1b83bc670128
Tim <horochowiktim@pm.me>
2023-12-05 19:12:44 +0100

    part 1 of day one finished

--- day1/main.c
+++ day1/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
+int32_t add_values(FILE* ptr) {
+    char file_char;
+    int32_t value = 0, digits[2];
+    bool first_value = true;
+
+    do {
+        file_char = fgetc(ptr);
+        if (isdigit(file_char)) {
+            if (first_value == true) {
+                digits[0] = file_char - '0';
+                first_value = false;
+            }
+            digits[1] = file_char - '0';
+        }
+        if (file_char == '\n') {
+            first_value = true;
+            value += digits[0] * 10 + digits[1];
+            printf("%d\n", value);
+        }
+    } while (file_char != EOF);
+    return value;
+}
+
+// Driver code
+int32_t main()
+{
+    FILE* ptr = fopen("document.txt", "r");
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+    printf("Sum of calibration values: %d", add_values(ptr));
+    fclose(ptr);
     return 0;
 }
