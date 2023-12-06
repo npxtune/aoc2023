2bb19e03ea1ae61a3484647515fb12649e668733
Tim <horochowiktim@pm.me>
2023-12-06 15:42:05 +0100

    day one finished

--- day1/main.c
+++ day1/main.c
@@ -2,36 +2,136 @@
 #include <ctype.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
-int64_t add_values(FILE* ptr) {
-    char file_char;
+#define MAX 100
+
+int64_t check_digits(FILE* ptr) {
+    char buffer[MAX];
     int64_t value = 0, digits[2];
     bool first_value = true;
 
-    do {
-        file_char = fgetc(ptr);
-        if (isdigit(file_char)) {
-            if (first_value == true) {
-                digits[0] = file_char - '0';
-                first_value = false;
+    while (!feof(ptr)) {
+        fgets(buffer, MAX, ptr);
+        printf("Line to check: %s", buffer);
+        for (int32_t i = 0; i < MAX; i++) {
+            if (isdigit(buffer[i])) {   // Check if there is a calibration digit
+                if (first_value == true) {
+                    digits[0] = buffer[i] - '0';
+                    first_value = false;
+                }
+                digits[1] = buffer[i] - '0';
+                } else {    // Check if there is a "character digit"
+                    switch (buffer[i]) {
+                        case 'o':   // 1
+                            if (strncmp(&buffer[i], "one", 3) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 1;
+                                    first_value = false;
+                                }
+                                digits[1] = 1;
+                            }
+                            break;
+
+                        case 't':   // 2, 3
+                            if (strncmp(&buffer[i], "two", 3) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 2;
+                                    first_value = false;
+                                }
+                                digits[1] = 2;
+                            } else if (strncmp(&buffer[i], "three", 5) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 3;
+                                    first_value = false;
+                                }
+                                digits[1] = 3;
+                            }
+                            break;
+
+                        case 'f':   // 4, 5
+                            if (strncmp(&buffer[i], "four", 4) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 4;
+                                    first_value = false;
+                                }
+                                digits[1] = 4;
+                            } else if (strncmp(&buffer[i], "five", 4) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 5;
+                                    first_value = false;
+                                }
+                                digits[1] = 5;
+                            }
+                            break;
+
+                        case 's':   // 6, 7
+                            if (strncmp(&buffer[i], "six", 3) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 6;
+                                    first_value = false;
+                                }
+                                digits[1] = 6;
+                            } else if (strncmp(&buffer[i], "seven", 5) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 7;
+                                    first_value = false;
+                                }
+                                digits[1] = 7;
+                            }
+                            break;
+
+                        case 'e':   // 8
+                            if (strncmp(&buffer[i], "eight", 5) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 8;
+                                    first_value = false;
+                                }
+                                digits[1] = 8;
+                            }
+                            break;
+
+                        case 'n':   // 9
+                            if (strncmp(&buffer[i], "nine", 4) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 9;
+                                    first_value = false;
+                                }
+                                digits[1] = 9;
+                            }
+                            break;
+
+                        case 'z':   // 0
+                            if (strncmp(&buffer[i], "zero", 4) == 0) {
+                                if (first_value == true) {
+                                    digits[0] = 0;
+                                    first_value = false;
+                                }
+                                digits[1] = 0;
+                            }
+                            break;
+
+                        default:    // no first char for digit found
+                            break;
+                    }
             }
-            digits[1] = file_char - '0';
+            if (buffer[i] == '\n' || buffer[i] == '\0') {
+                first_value = true;
+                value += digits[0] * 10 + digits[1];
+                break;
+            }
         }
-        if (file_char == '\n') {
-            first_value = true;
-            value += digits[0] * 10 + digits[1];
-        }
-    } while (file_char != EOF);
+    }
     return value;
 }
 
-int32_t main()
-{
-    FILE* ptr = fopen("document.txt", "r");
+int32_t main() {
+    FILE* ptr = fopen("input", "r");
     if (NULL == ptr) {
+        printf("Error, can't read file content\n");
         return -1;
     }
-    printf("Sum of calibration values: %ld", add_values(ptr));
+    printf("\n%ld\n", check_digits(ptr));
     fclose(ptr);
     return 0;
 }
