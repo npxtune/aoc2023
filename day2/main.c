17942af68559ba15169d535e30d77dcf78b52d3e
Tim <horochowiktim@pm.me>
2023-12-07 10:34:30 +0100

    day2 cleanup

--- day2/main.c
+++ day2/main.c
@@ -3,52 +3,47 @@
 #include <ctype.h>
 #include <math.h>
 #include <string.h>
+#include "struct_types.h"
 
 #define MAXLINE 200
 
-typedef struct {
-    uint32_t red;
-    uint32_t green;
-    uint32_t blue;
-} cube_colours;
-
-typedef struct {
-    char* word;
-} lookup_t;
-
-uint32_t get_digit(char line_buffer[], const int32_t index) {
+uint32_t parse_digit(char line_buffer[], const int32_t index) {   // "Parse" the digit
     if (line_buffer[index-3] == ' ') {
         return digittoint(line_buffer[index-2]);
     }
     return digittoint(line_buffer[index-3]) * 10 + digittoint(line_buffer[index-2]);
 }
 
-void check_cubes(FILE* ptr, const cube_colours limit, lookup_t words[]) {
-    char file_buffer[MAXLINE];
-    uint32_t sum_game_id = 0, current_game_id = 1, game_powers = 0;
-    while (!feof(ptr)) {
-        cube_colours max_digit = {0,0,0};
-        fgets(file_buffer, MAXLINE, ptr);
-        for (int32_t i = 0; file_buffer[i] != '\0'; i++) {
-            for (int32_t j = 0; j < 3; j++) {
-                if (strncmp(words[j].word, &file_buffer[i], strlen(words[j].word)) == 0) {
-                    const uint32_t current_digit = get_digit(file_buffer, i);
-                    switch (j) {
-                        case 0: // blue
-                            max_digit.blue = fmax(current_digit, max_digit.blue);
-                            break;
-                        case 1: // red
-                            max_digit.red = fmax(current_digit, max_digit.red);
-                            break;
-                        case 2: // green
-                            max_digit.green = fmax(current_digit, max_digit.green);
-                            break;
-                        default:
-                            break;
-                    }
+cube_colours get_digit(char file_buffer[], lookup_t words[]) {
+    cube_colours max_digit = {0,0,0};
+    for (int32_t i = 0; file_buffer[i] != '\0'; i++) {  // Check the entire line in the file buffer for matching words
+        for (int32_t j = 0; j < 3; j++) {
+            if (strncmp(words[j].word, &file_buffer[i], strlen(words[j].word)) == 0) {
+                const uint32_t current_digit = parse_digit(file_buffer, i);
+                switch (j) {
+                    case 0: // blue
+                        max_digit.blue = fmax(current_digit, max_digit.blue);
+                    break;
+                    case 1: // red
+                        max_digit.red = fmax(current_digit, max_digit.red);
+                    break;
+                    case 2: // green
+                        max_digit.green = fmax(current_digit, max_digit.green);
+                    break;
+                    default:
+                        break;
                 }
             }
         }
+    }
+    return max_digit;
+}
+
+void check_input(FILE* ptr, const cube_colours limit, lookup_t words[]) {
+    uint32_t sum_game_id = 0, current_game_id = 1, game_powers = 0;
+    while (!feof(ptr)) {
+        char* file_buffer = fgets(file_buffer, MAXLINE, ptr);
+        const cube_colours max_digit = get_digit(file_buffer, words); // Get matching words + digits from the input file
         if (max_digit.red <= limit.red && max_digit.blue <= limit.blue && max_digit.green <= limit.green) {
             sum_game_id += current_game_id++;
         }
@@ -59,16 +54,11 @@ void check_cubes(FILE* ptr, const cube_colours limit, lookup_t words[]) {
 }
 
 int32_t main() {
-    const lookup_t words[] = {    // Words to check for
-        {"blue"},
-        {"red"},
-        {"green"}
-    };
     FILE* ptr = fopen("input", "r");
     if (ptr == NULL) {
         printf("Error, can't read file content!\n");
         return -1;
     }
-    check_cubes(ptr, (cube_colours){12, 13, 14}, words);
+    check_input(ptr, (cube_colours){12, 13, 14}, (lookup_t[]){{"blue"}, {"red"}, {"green"}});
     return 0;
 }
