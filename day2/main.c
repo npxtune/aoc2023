0eee5464bbe458170ffcf498b8b2fd2b5da35147
Tim <horochowiktim@pm.me>
2023-12-07 09:53:31 +0100

    day2 cleanup

--- day2/main.c
+++ day2/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <math.h>
 #include <string.h>
-#include <stdbool.h>
 
 #define MAXLINE 200
 
@@ -27,7 +27,6 @@ void check_cubes(FILE* ptr, const cube_colours limit, lookup_t words[]) {
     char file_buffer[MAXLINE];
     uint32_t sum_game_id = 0, current_game_id = 1, game_powers = 0;
     while (!feof(ptr)) {
-        bool is_valid = true;
         cube_colours minimum_digit = {0,0,0};
         fgets(file_buffer, MAXLINE, ptr);
         for (int32_t i = 0; file_buffer[i] != '\0'; i++) {
@@ -36,16 +35,13 @@ void check_cubes(FILE* ptr, const cube_colours limit, lookup_t words[]) {
                     const uint32_t current_digit = get_digit(file_buffer, i);
                     switch (j) {
                         case 0: // blue
-                            current_digit > minimum_digit.blue ? minimum_digit.blue = current_digit : 0;
-                            current_digit > limit.blue ? is_valid = false : 0;
+                            minimum_digit.blue = fmax(current_digit, minimum_digit.blue);
                             break;
                         case 1: // red
-                            current_digit > minimum_digit.red ? minimum_digit.red = current_digit : 0;
-                            current_digit > limit.red ? is_valid = false : 0;
+                            minimum_digit.red = fmax(current_digit, minimum_digit.red);
                             break;
                         case 2: // green
-                            current_digit > minimum_digit.green ? minimum_digit.green = current_digit : 0;
-                            current_digit > limit.green ? is_valid = false : 0;
+                            minimum_digit.green = fmax(current_digit, minimum_digit.green);
                             break;
                         default:
                             break;
@@ -53,7 +49,7 @@ void check_cubes(FILE* ptr, const cube_colours limit, lookup_t words[]) {
                 }
             }
         }
-        if (is_valid == true) {
+        if (minimum_digit.red <= limit.red && minimum_digit.blue <= limit.blue && minimum_digit.green <= limit.green) {
             sum_game_id += current_game_id++;
         }
         game_powers += minimum_digit.red * minimum_digit.green * minimum_digit.blue;
