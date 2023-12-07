8cabf3768e906009a9bb4e57a41056898106005e
Tim <horochowiktim@pm.me>
2023-12-07 10:34:40 +0100

    day2 cleanup

---
+++ day2/struct_types.h
@@ -0,0 +1,15 @@
+
+#ifndef STRUCT_TYPES_H
+#define STRUCT_TYPES_H
+
+#endif //STRUCT_TYPES_H
+
+typedef struct {
+    uint32_t red;
+    uint32_t green;
+    uint32_t blue;
+} cube_colours;
+
+typedef struct {
+    char* word;
+} lookup_t;
